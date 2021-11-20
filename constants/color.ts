851279af4ab9ce88c84e1762cc8425ccf0226c77
Hisam Fahri <hisam@alphaaiauto.com>
2021-11-20 13:06:57 +0700

    feat(constants): add colors object

---
+++ constants/color.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+    darkBackground: '#1D1F21',
+    mainGreen: '#2CBC8A',
+    textGrey: '#C9CACC',
+    secondaryTextGrey: '#666666',
+    hoversecondaryTextGrey: '#858585',
+    hoverPink: '#D480AA',
+    white: '#FFFFFF',
+    black: '#000000'
+}
