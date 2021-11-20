d8f7e15c24025524b387f8fe8153933b855dd99a
Hisam Fahri <hisam@alphaaiauto.com>
2021-11-20 13:51:43 +0700

    feat(fonts): add fonts style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { COLORS } from '../constants/color'
-import styles from '../styles/Home.module.css'
+import globalStyle from '../styles/Global.module.css'
+import fontStyle from '../styles/Fonts.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Hisam Fahri</title>
         <meta name="description" content="Hisam Fahri's Personal Website" />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main>
-        <p style={{color: COLORS.white, fontFamily: "'IBM Plex Mono', 'monospace'"}}># Hello, world!</p>
+      <main className={globalStyle.main}>
+        <p className={fontStyle.textSubBase} style={{ color: COLORS.textGrey }}>.textSubBase</p>
+        <p className={fontStyle.textBase} style={{ color: COLORS.textGrey }}>.textBase</p>
+        <p className={fontStyle.textH3} style={{ color: COLORS.textGrey }}>.textH3</p>
+        <p className={fontStyle.textH2} style={{ color: COLORS.textGrey }}>.textH2</p>
+        <p className={fontStyle.textSubBaseUnderlined} style={{ color: COLORS.textGrey }}>.textSubBaseUnderlined</p>
+        <p className={fontStyle.textBaseUnderlined} style={{ color: COLORS.textGrey }}>.textBaseUnderlined</p>
+        <p className={fontStyle.textH3Underlined} style={{ color: COLORS.textGrey }}>.textH3Underlined</p>
+        <p className={fontStyle.textH2Underlined} style={{ color: COLORS.textGrey }}>.textH2Underlined</p>
       </main>
     </div>
   )
