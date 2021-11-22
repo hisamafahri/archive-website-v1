d2b5fea120d2ba5e5ec930015cae37163a8267c6
Hisam Fahri <hisam@alphaaiauto.com>
2021-11-22 13:43:12 +0700

    feat(header): update link

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { COLORS } from '../constants/color'
 import homeStyle from '../styles/Home.module.css'
 import fontStyle from '../styles/Fonts.module.css'
 import Logo from '../assets/svg/Logo'
+import Link from 'next/link'
 
 export default function Header({ }: any): ReactElement {
     return (
@@ -11,8 +12,18 @@ export default function Header({ }: any): ReactElement {
                 <Logo />
             </div>
             <div className={homeStyle.navigator}>
-                <p className={fontStyle.textH2} style={{ color: COLORS.white, margin: 0 }}>Hisam Fahri</p>
-                <p className={fontStyle.textBase} style={{ color: COLORS.mainGreen, marginTop: '4px', marginBottom: 0 }}><span className={homeStyle.headerNav} onClick={() => { }}>Home</span>  |  <span className={homeStyle.headerNav} onClick={() => { }}>About</span>  |  <span className={homeStyle.headerNav} onClick={() => { }}>Writing</span>  |  <span className={homeStyle.headerNav} onClick={() => { }}>Project</span></p>
+                <p className={fontStyle.textH2} style={{ color: COLORS.textGrey, margin: 0 }}>Hisam Fahri</p>
+                <p className={fontStyle.textBase} style={{ color: COLORS.mainGreen, marginTop: '4px', marginBottom: 0 }}>
+                    <Link href='/'>
+                        <span className={homeStyle.headerNav} onClick={() => { }}>Home</span>
+                    </Link>  | <Link href='/about'>
+                        <span className={homeStyle.headerNav} onClick={() => { }}>About</span>
+                    </Link>  |  <Link href='/writing'>
+                        <span className={homeStyle.headerNav} onClick={() => { }}>Writing</span>
+                    </Link>  |  <Link href='/project'>
+                        <span className={homeStyle.headerNav} onClick={() => { }}>Project</span>
+                    </Link>
+                </p>
             </div>
         </div>
     )
