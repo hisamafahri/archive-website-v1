e3e59bfd862f4e1c91d6a149952295bf7351da65
Hisam Fahri <hisam@alphaaiauto.com>
2021-11-22 13:20:18 +0700

    feat(home): add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { COLORS } from '../constants/color'
 import globalStyle from '../styles/Global.module.css'
+import homeStyle from '../styles/Home.module.css'
 import fontStyle from '../styles/Fonts.module.css'
+import Logo from '../assets/svg/Logo'
 
 const Home: NextPage = () => {
   return (
@@ -14,14 +16,15 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={globalStyle.main}>
-        <p className={fontStyle.textSubBase} style={{ color: COLORS.textGrey }}>.textSubBase</p>
-        <p className={fontStyle.textBase} style={{ color: COLORS.textGrey }}>.textBase</p>
-        <p className={fontStyle.textH3} style={{ color: COLORS.textGrey }}>.textH3</p>
-        <p className={fontStyle.textH2} style={{ color: COLORS.textGrey }}>.textH2</p>
-        <p className={fontStyle.textSubBaseUnderlined} style={{ color: COLORS.textGrey }}>.textSubBaseUnderlined</p>
-        <p className={fontStyle.textBaseUnderlined} style={{ color: COLORS.textGrey }}>.textBaseUnderlined</p>
-        <p className={fontStyle.textH3Underlined} style={{ color: COLORS.textGrey }}>.textH3Underlined</p>
-        <p className={fontStyle.textH2Underlined} style={{ color: COLORS.textGrey }}>.textH2Underlined</p>
+        <div className={homeStyle.header}>
+          <div onClick={() => { }} className={homeStyle.logo}>
+            <Logo />
+          </div>
+          <div className={homeStyle.navigator}>
+            <p className={fontStyle.textH2} style={{ color: COLORS.white, margin: 0 }}>Hisam Fahri</p>
+            <p className={fontStyle.textBase} style={{ color: COLORS.mainGreen, marginTop: '4px', marginBottom: 0 }}><span className={homeStyle.headerNav} onClick={() => {}}>Home</span>  |  <span className={homeStyle.headerNav} onClick={() => {}}>About</span>  |  <span className={homeStyle.headerNav} onClick={() => {}}>Writing</span>  |  <span className={homeStyle.headerNav} onClick={() => {}}>Project</span></p>
+          </div>
+        </div>
       </main>
     </div>
   )
