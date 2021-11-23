12bd7a94acb90abc95d89abc949f241e77c6a215
Hisam Fahri <hisam@alphaaiauto.com>
2021-11-23 21:23:33 +0700

    feat(redirect): add redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/work',
+        destination: '/project',
+        permanent: true,
+      },
+      {
+        source: '/works',
+        destination: '/project',
+        permanent: true,
+      },
+      {
+        source: '/contact',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
