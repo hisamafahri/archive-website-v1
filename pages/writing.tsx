7425bb963d39a746c9f4f163b3fa1511436273ea
Hisam Fahri <hisam@alphaaiauto.com>
2021-11-23 20:45:19 +0700

    feat(writing): add writing page

---
+++ pages/writing.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { COLORS } from '../constants/color'
+import globalStyle from '../styles/Global.module.css'
+import fontStyle from '../styles/Fonts.module.css'
+import Header from '../components/Header'
+import Image from 'next/image'
+import Footer from '../components/Footer'
+
+const Writing: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Writing</title>
+                <meta name="description" content="Hisam Fahri's Personal Website" />
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+
+            <main className={globalStyle.main}>
+                <Header title='Writing' />
+                <div className={fontStyle.textH2} style={{ color: COLORS.textGrey, textAlign: 'center' }}>
+                    <p>Coming soon...</p>
+                </div>
+                <Footer />
+            </main>
+        </div>
+    )
+}
+
+export default Writing
